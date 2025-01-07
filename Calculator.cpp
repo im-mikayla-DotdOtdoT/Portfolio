1d317d670aa4b080efe39e540082fe908b8cb811
im_mikayla_DotdOtdoT <mpenn287@gmail.com>
2025-01-07 17:56:46 -0500

    Add files via upload
    Calculator using else if statements.

---
+++ Calculator.cpp
@@ -0,0 +1,41 @@
+// display calculator title
+// have user input a number
+// have user input an operation
+// have user input another number
+// display result
+
+#include <iostream>
+
+int main()
+{
+    float num1, num2;
+	char operation;
+	std::cout << "Calculator" << std::endl;
+	std::cout << "Enter a number: ";
+	std::cin >> num1;
+	std::cout << "Enter an operation: ";
+	std::cin >> operation;
+	std::cout << "Enter another number: ";
+	std::cin >> num2;
+	if (operation == '+')
+	{
+		std::cout << num1 + num2;
+	}
+	else if (operation == '-')
+	{
+		std::cout << num1 - num2;
+	}
+	else if (operation == '*')
+	{
+		std::cout << num1 * num2;
+	}
+	else if (operation == '/')
+	{
+		std::cout << num1 / num2;
+	}
+	else
+	{
+		std::cout << "Invalid operation";
+	}
+	return 0;
+}
