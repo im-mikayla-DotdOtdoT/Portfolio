c32268bc8c46efcc6a5c2d8a991d0097be2bb5d9
im_mikayla_DotdOtdoT <mpenn287@gmail.com>
2025-03-03 18:15:04 -0500

    Delete shopping_cart_program.py

--- shopping_cart_program.py
+++
@@ -1,7 +0,0 @@
-# Exercise 2: Shopping Cart Program
-
-item = input("What item would you like to purchase?: ")
-price = float(input("What is the price?: ")) # type cast input as float
-quantity = int(input("How many would you like?: ")) # type cast as integer
-total = price * quantity # get price total by mult.
-print(f"Your total price for {quantity} {item} is: {total}.")
